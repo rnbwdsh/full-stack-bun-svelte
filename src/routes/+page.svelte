c520a7650492d3f0f71b1f17252110a4016aaab4
Markus <vogl91@gmail.com>
2024-06-03 16:29:39 +0200

    initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
@@ -10,14 +8,8 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />SvelteKit app
+		Welcome to your new<br />SvelteKit app
 	</h1>
 
 	<h2>
